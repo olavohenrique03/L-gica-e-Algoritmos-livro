0e21f7d29f8e08391dde08ee755b5f9b5c13d453
olavohenrique03 <olavo060103@gmail.com>
2026-06-10 21:12:17 -0300

    exercise solutions resp5_c (cap 5 Finished)

---
+++ cap05/js/resp5_c.js
@@ -0,0 +1,28 @@
+const frm = document.querySelector("form");
+const resp1 = document.querySelector("#resp1");
+const resp2 = document.querySelector("#resp2");
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const num = Number(frm.inNumero.value);
+
+    let divisores = 1;
+    let resultado = `Divisores do ${num}: 1`
+
+    for (let i = 2; i < num; i++) {
+
+        if (num % i === 0) {
+            resultado += `, ${i}`;
+            divisores += i 
+        }
+
+    }
+    resp1.innerText = `${resultado} (Soma: ${divisores})`;
+
+    if (divisores === num) {
+        resp2.innerText = `${num} É um Número Perfeito`;
+    } else {
+        resp2.innerText = `${num} Não é um Número Perfeito`;
+    }
+})
